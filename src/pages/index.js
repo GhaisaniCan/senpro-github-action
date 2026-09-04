9e4874d1582fe31d8bc5fc453a50020ab6cb1405
GhaisaniCan <ghaisanrifqikamiel@mail.ugm.ac.id>
2026-09-04 09:38:37 +0700

    case 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+// import Head from 'next/head'
+// import Image from 'next/image'
+// import { Inter } from '@next/font/google'
+// import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -28,9 +28,10 @@ export default function Home() {
           </h1>
         </div>
 
+
         <div>
           <h5 className={inter.className}>
-            [Nama] | [NIU]
+            Ghaisan Rifqi Kamiel | 540091
           </h5>
         </div>
       </main>
